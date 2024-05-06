7319e22ada71a232018d5a3b53ed779403000e86
Sujin <sk1087sk@gmail.com>
2024-05-06 16:56:09 +0200

    03

--- myContacts/app.js
+++ myContacts/app.js
@@ -3,6 +3,13 @@ const dbConnect = require("./config/dbConnect");
 
 const app = express();
 
+// 내가 사용할 템플릿이 무엇이고, 템플릿 파일이 어디에 저장되는지 알려줌
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// 정적파일 위치 지정
+app.use(express.static("./public"));
+
 // 데이터베이스에 접속
 dbConnect();
 
--- myContacts/controllers/contactController.js
+++ myContacts/controllers/contactController.js
@@ -1,19 +1,17 @@
-// get all contacts
-// get /contacts
-// const getAllContacts = async (req, res) => {
-//   try {
-//     res.send("contacts page");
-//   } catch (error) {
-//     res.send(error);
-//   }
-// };
-
-// asyncHandler로 재작성
 const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
 
 // try catch 안 써도 됨
 const getAllContacts = asyncHandler(async (req, res) => {
-  res.send("contacts page");
+  const contacts = await Contact.find();
+  const users = [
+    { name: "jang", email: "jang@gmail.com", phone: "78945" },
+    { name: "jin", email: "jin@gmail.com", phone: "32345" },
+  ];
+
+  // 템플릿 파일을 표시할 때는 res.render 함수
+  // res.render("getAll.ejs");
+  res.render("getAll", { users: users });
 });
 
 // create contact
@@ -24,22 +22,42 @@ const createContact = asyncHandler(async (req, res) => {
   if (!name || !email || !phone) {
     return res.send("필수 값이 입력되지 않았습니다.");
   }
+  const contact = await Contact.create({ name, email, phone });
   res.send("Create Contacts");
 });
 
 // get /contacts/:id
 const getContact = asyncHandler(async (req, res) => {
-  res.send(`View contact for ID: ${req.params.id}`);
+  const contact = await Contact.findById(req.params.id);
+  res.send(contact);
 });
 
 // put /contacts/:id
 const modifyContact = asyncHandler(async (req, res) => {
-  res.send(`Update contact for ID: ${req.params.id}`);
+  const id = req.params.id;
+  const { name, email, phone } = req.body;
+  const contact = await Contact.findById(id);
+  if (!contact) {
+    throw new Error("Contact not found");
+  }
+  contact.name = name;
+  contact.email = email;
+  contact.phone = phone;
+
+  contact.save();
+
+  res.json(contact);
 });
 
 // delete /contacts/:id
 const deleteContact = asyncHandler(async (req, res) => {
-  res.send(`Delte contact for ID: ${req.params.id}`);
+  const id = req.params.id;
+  const contact = await Contact.findById(id);
+  if (!contact) {
+    throw new Error("Contact not found");
+  }
+  await Contact.deleteOne(contact);
+  res.send(`Deleted`);
 });
 
 module.exports = {
